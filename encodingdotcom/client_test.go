ede476fc9a2edab446d6e13e2e73f177e9a81ce1
Thompson Marzagao <thompson.marzagao@nytimes.com>
2016-01-14 14:17:51 -0800

    Initial client version
    * Includes request struct
    * And implementation for custom booleans

---
+++ encodingdotcom/client_test.go
@@ -0,0 +1,27 @@
+package encodingdotcom
+
+import (
+  "encoding/json"
+  "testing"
+
+  "gopkg.in/check.v1"
+)
+
+type S struct{}
+
+func Test(t *testing.T) {
+  check.TestingT(t)
+}
+
+var _ = check.Suite(&S{})
+
+func (s *S) TestYesNoBoolean(c *check.C) {
+  bTrue := YesNoBoolean(true)
+  bFalse := YesNoBoolean(false)
+  data, err := json.Marshal(bTrue)
+  c.Assert(err, check.IsNil)
+  c.Assert(string(data), check.Equals, `"yes"`)
+  data, err = json.Marshal(bFalse)
+  c.Assert(err, check.IsNil)
+  c.Assert(string(data), check.Equals, `"no"`)
+}
