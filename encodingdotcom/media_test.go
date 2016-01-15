c561b2aa410102a2639f878d602830aa377a9f5d
Francisco Souza <francisco.souza@nytimes.com>
2016-01-15 11:04:57 -0500

    Add help method for starting the fake server

--- encodingdotcom/media_test.go
+++ encodingdotcom/media_test.go
@@ -1,22 +1,11 @@
 package encodingdotcom
 
 import (
-	"net/http"
-	"net/http/httptest"
-
 	"gopkg.in/check.v1"
 )
 
 func (s *S) TestAddMedia(c *check.C) {
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`{
-				"response": {
-				"message": "Added",
-				"MediaID": "1234567"
-				}
-		}`))
-	}))
-
+	server := s.startServer(`{"response": {"message": "Added", "MediaID": "1234567"}}`, 200)
 	defer server.Close()
 	client := Client{Endpoint: server.URL, UserID: "myuser", UserKey: "123"}
 	addMediaResponse, err := client.AddMedia([]string{"http://another.non.existent/video.mov"}, &Format{
--- encodingdotcom/suite_test.go
+++ encodingdotcom/suite_test.go
@@ -1,6 +1,8 @@
 package encodingdotcom
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"gopkg.in/check.v1"
@@ -13,3 +15,11 @@ func Test(t *testing.T) {
 }
 
 var _ = check.Suite(&S{})
+
+func (s *S) startServer(content string, status int) *httptest.Server {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(status)
+		w.Write([]byte(content))
+	}))
+	return server
+}
